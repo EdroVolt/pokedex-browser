7f6c49d82a0e2420c40c83e551e148467168ea81
EdroVolt <ahmededrees30@gmail.com>
2025-07-26 04:24:04 +0300

    Add Tailwind CSS and Shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <>
+  <div className="bg-red-500">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+  </>;
 }
 
 export default App;
